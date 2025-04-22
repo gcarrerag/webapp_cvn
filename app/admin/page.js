663a354415e6d93d00740434ed02902e1b6ec802
Gerard <gerard.cagu@gmail.com>
2025-04-22 11:28:06 +0200

    esperem q funciona be ara pls

--- app/admin/page.js
+++ app/admin/page.js
@@ -180,9 +180,15 @@ function Admin() {
 
         {/* Capçalera i sessió */}
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold">Carla Carrera Panell d&apos;administració</h1>
-          <button onClick={tancarSessio} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Tancar sessió</button>
-        </div>
+	  <h1 className="text-2xl font-bold">👩 Carla Carrera⚕️ Panell d'administració</h1>
+	  <button
+	    onClick={tancarSessio}
+	    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+	  >
+	    Tancar sessió
+	  </button>
+	</div>
+
 
         {/* Formulari de productes */}
         <form onSubmit={afegirProducte} className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-10 bg-white p-6 rounded shadow">
