f7a724ae6b0d06c5d0194d194271868d36236365
Gerard <gerard.cagu@gmail.com>
2025-04-24 20:26:05 +0200

    web guapa

--- app/layout.js
+++ app/layout.js
@@ -14,20 +14,15 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Centre Veterinari Navarcles",
   description: "Compromesos amb el benestar de la teva mascota",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ca">
-      <head>
-        {/* 🔵 Afegeix el favicon personalitzat */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
