d18d01c6a1c227cf0768b34f3494a4899c48ec04
Gerard <gerard.cagu@gmail.com>
2025-05-01 11:31:36 +0200

    Estil modificat a tota la web

--- app/api/contacte/route.js
+++ app/api/contacte/route.js
@@ -4,7 +4,8 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
 
-  const { name, email, phone, message } = await req.json();
+  const { nom, email, telefon, missatge, tipus } = await req.json();
+
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -15,17 +16,19 @@ export async function POST(req) {
   });
 
   const mailOptions = {
-    from: `"Formulari Web - ${name}" <${process.env.EMAIL_USER}>`,
-    to: process.env.EMAIL_TO,
-    replyTo: email,
-    subject: `Nou missatge de ${name}`,
-    html: `
-      <p><strong>Nom:</strong> ${name}</p>
-      <p><strong>Telèfon:</strong> ${phone}</p>
-      <p><strong>Correu electrònic:</strong> ${email}</p>
-      <p><strong>Missatge:</strong><br/>${message}</p>
-    `,
-  };
+	  from: `"Formulari Web - ${nom}" <${process.env.EMAIL_USER}>`,
+	  to: process.env.EMAIL_TO,
+	  replyTo: email,
+	  subject: `Nou missatge de ${nom}`,
+	  html: `
+	    <p><strong>Nom:</strong> ${nom}</p>
+	    <p><strong>Telèfon:</strong> ${telefon}</p>
+	    <p><strong>Correu electrònic:</strong> ${email}</p>
+	    <p><strong>Tipus de consulta:</strong> ${tipus}</p>
+	    <p><strong>Missatge:</strong><br/>${missatge}</p>
+	  `,
+	}
+
 
   try {
     await transporter.sendMail(mailOptions);
